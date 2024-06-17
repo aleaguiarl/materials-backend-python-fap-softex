adbf4ef358bada9a9b5fef1a9c9160d2172c323f
aleaguiarl <venom4008@gmail.com>
2024-06-17 15:58:53 -0300

    Added exercise 5 from week 3

---
+++ exercises/week_3/ex_5.py
@@ -0,0 +1,5 @@
+height = float(input("Insert the height in meters(ex. 1.76): "))
+ideal_weight = 72.7 * height - 58
+print(f"\n+------------------------------+")
+print(f"The ideal weight for a person {height}m tall is {ideal_weight:.2f}kg")
+print(f"+------------------------------+")
