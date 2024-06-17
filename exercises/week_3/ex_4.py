e3c284c4ca8d93ef92b4e477aaea8daeddb8b6e5
aleaguiarl <venom4008@gmail.com>
2024-06-17 15:55:37 -0300

    Added exercise 4 from week 3

---
+++ exercises/week_3/ex_4.py
@@ -0,0 +1,5 @@
+fahrenheit = float(input("Insert the temperature in fahrenheit: "))
+celsius = (fahrenheit - 32 ) * 5/9
+print(f"+------------------------------+")
+print(f"{fahrenheit:.2f} °F is equivalent to {celsius:.2f} °C")
+print(f"+------------------------------+")
